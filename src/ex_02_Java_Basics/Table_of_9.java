ab3b574da4cc3bb50f3a3fe4b03fac4609bc15af
pandeyshubham <pandeyshubham4838@gmail.com>
2026-01-03 15:33:23 +0530

    Add file

--- src/ex_02_Java_Basics/Table_of_9.java
+++ src/ex_02_Java_Basics/Table_of_9.java
@@ -14,6 +14,18 @@ public class Table_of_9 {
         System.out.printf("%d * 9 = %d%n" , a , a*9);
         System.out.printf("%d * 10 = %d%n" , a , a*10);
 
+        int b = 5;
+        System.out.printf("%d * 1 = %d%n" , b , b*1);
+        System.out.printf("%d * 2 = %d%n" , b  , b*2);
+        System.out.printf("%d * 3 = %d%n" , b  , b*3);
+        System.out.printf("%d * 4 = %d%n" , b , b*4);
+        System.out.printf("%d * 5 = %d%n" , b , b*5);
+        System.out.printf("%d * 6 = %d%n" , b , b*6);
+        System.out.printf("%d * 7 = %d%n" , b , b*7);
+        System.out.printf("%d * 8 = %d%n" , b , b*8);
+        System.out.printf("%d * 9 = %d%n" , b , b*9);
+        System.out.printf("%d * 10 = %d%n" , b , b*10);
+
 
     }
     }
